9c982ecd41d7c45774bbb8f9b4ecb23f75ac5fe7
MaximilianoNaveillan <maximilianonaveillan@gmail.com>
2025-01-27 02:34:16 -0300

    add open port in app.py

--- backend/app.py
+++ backend/app.py
@@ -251,4 +251,5 @@ def delete_asesoria(asesoria_id):
     
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
